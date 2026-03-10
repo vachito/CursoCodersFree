178d3988fcd68bd01419bafc1861bce4c131143f
Rigoberto Xocoxic <vacho19@gmail.com>
2026-03-09 19:19:51 -0600

    video 14 como proteger rutas con expresiones regulares

--- routes/web.php
+++ routes/web.php
@@ -15,15 +15,40 @@ Route::get('/cursos/informacion', function(){
    return "Aqui podras encontrar toda la informacion de los cursos"; 
 });
 
-Route::get('/cursos/{curso}', function($curso){
-    return "Bienvenido al curso: $curso";
+//ruta con expresion regular para recibir un parametro unicamente numerico
+Route::get('/cursos/{id}', function($id){
+    return "Bienvenido al curso: $id";
 });
 
-// ruta con dos parametros, uno variable y otro opciona
-// Route::get('/cursos/{curso}/{categoria?}', function($curso,$categoria=null){
-//     if($categoria){
-//         return "Bienvenido al curso: $curso de la categoria $categoria"; 
-//     }
-
+//ruta con expresion regular para recibir un parametro unicamente numerico
+// Route::get('/cursos/{curso}', function($curso){
 //     return "Bienvenido al curso: $curso";
-// });
+// })->whereIn('curso',['laravel','php','vue']);
+
+//ruta con expresion regular para recibir mas de un parametro
+// Route::get('/cursos/{curso}/{categoria}', function($curso,$categoria){
+//     return "Bienvenido al curso: $curso de la categoria $categoria";
+// })->where([
+//         'curso' => '[A-Za-z]+',
+//         'categoria' => '[A-Za-z]+'
+//     ]);
+
+//ruta con expresion regular para recibir un parametro unicamente numerico
+// Route::get('/cursos/{curso}', function($curso){
+//     return "Bienvenido al curso: $curso";
+// })->where('curso','[0-9]+');
+
+//ruta con expresion regular para recibir un parametro unicamente numerico
+// Route::get('/cursos/{curso}', function($curso){
+//     return "Bienvenido al curso: $curso";
+// })->whereAlpha('curso');
+
+//ruta con expresion regular para recibir un parametro unicamente numerico
+// Route::get('/cursos/{curso}', function($curso){
+//     return "Bienvenido al curso: $curso";
+// })->whereAlphaNumeric('curso');
+
+//ruta con expresion regular para recibir un parametro unicamente alfabetico
+// Route::get('/cursos/{curso}', function($curso){
+//     return "Bienvenido al curso: $curso";
+// })->where('curso','[A-Za-z]+');
