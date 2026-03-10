4ab59b49496514c47830a1461e66322383e09726
Rigoberto Xocoxic <vacho19@gmail.com>
2026-03-09 18:31:17 -0600

    video 12, definir rutas

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Hola desde la pagina de inicio";
+    //return view('welcome');
+});
+
+// Route::get('/contacto', function(){
+//    return "Hola desde la pagina de contacto"; 
+// });
+
+// Route::post('/contacto', function(){
+//    return "Hola desde la pagina de contacto"; 
+// });
+
+Route::match(['get','post'],'/contacto', function(){
+    return "Hola desde la pagina de contacto usando el metodo get o post";
 });
