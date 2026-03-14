34850f9d27dcf9669193385641f517d07bdcd16d
Rigoberto Xocoxic <vacho19@gmail.com>
2026-03-14 10:06:47 -0600

    video 27 como crear y registrar un provider

---
+++ app/Providers/ViewServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+class ViewServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //envia un parametro a todas las vistas
+        View::share('msg','VachoCode en laravel');
+    }
+}
